ca8cad56da895a0a75c93234bb7c1fb0c6588b44
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-01-04 18:06:02 +0800

    chrome: update all for linux

--- google-chrome/upstream-info.nix
+++ google-chrome/upstream-info.nix
@@ -1,20 +1,20 @@
 {
   beta = {
-    hash_deb_amd64 = "sha256-ALlUyjpR626Bc751avo2W/23+DVP2/oSmf2Cbbl/mz8=";
-    version = "131.0.6778.33";
+    hash_deb_amd64 = "sha256-qVN9RPGmpSyGXpvZ+zKHeVTmiNG7hFh6waVVmuI8ftc=";
+    version = "132.0.6834.57";
   };
   dev = {
-    hash_deb_amd64 = "sha256-Us4pxFKfXw/j0/BIsEEcDwHRQ1VOB/1oUzulmabHslI=";
-    version = "132.0.6821.2";
+    hash_deb_amd64 = "sha256-z9RfN9/OIkmBphnl80ezDnY19Z/bNMau0depdUeC1fQ=";
+    version = "133.0.6905.0";
   };
   stable = {
     chromedriver = {
       hash_darwin = "sha256-PkrgM6CcpaXgfsL6DFRy8PWNuO56BUDASXCadLDnFjo=";
       hash_darwin_aarch64 = "sha256-/AMvzZLDeK3EK/8PnlWOIAG0v/d6m6ybmsCJqbOaed4=";
-      hash_linux = "sha256-WjdzlX+l2SyzIEYpN+CpARZ4z4na2zt/rCvMlLvwHZo=";
-      version = "130.0.6723.93";
+      hash_linux = "sha256-vAZUFufRfvkRsbXnqWD4zE3hgTWbhFqDlauXN7m6mIw=";
+      version = "131.0.6778.204";
     };
-    hash_deb_amd64 = "sha256-HvLPjQSZk4zmQXoxhxujoWAZ8k0qivCyCdLpBxOJ4OA=";
-    version = "130.0.6723.116";
+    hash_deb_amd64 = "sha256-vAZUFufRfvkRsbXnqWD4zE3hgTWbhFqDlauXN7m6mIw=";
+    version = "131.0.6778.204";
   };
 }
